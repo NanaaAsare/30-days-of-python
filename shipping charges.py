6d1034941e0e1017c8b2f0fe2d6855feab2416c1
NanaaAsare <124165950+NanaaAsare@users.noreply.github.com>
2025-12-18 22:53:44 +0000

    Add files via upload

--- shipping charges.py
+++ shipping charges.py
@@ -1,14 +1,19 @@
 #ask the user of the weight of the package
 
-weight_of_package = int(input("Enter the weight of the car: "))
+weight_of_package = float(input("Enter the weight of the car: "))
 
 if weight_of_package <= 2:
-    print("your charge is $ 1.50")
-elif 2 > weight_of_package < 6:
-    print("your charge is $ 3.00")
-elif 6 > weight_of_package < 10:
-    print("Your charge is $ 4.00")
-elif weight_of_package > 10:
-    print("Your charge is $ 4.75")
+    rate = 1.50
+elif weight_of_package <= 6:
+    rate = 3.00
+elif weight_of_package <= 10:
+    rate = 4.00
+else:
+    rate = 4.75
 
+#calculating shipping charges
+shipping_charges = weight_of_package * rate
+
+#Displaynig the result
 print(f"Your weight is {weight_of_package:.2f}")
+print(f"Your shipping charge is {shipping_charges:.2f}")
